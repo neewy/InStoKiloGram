9c65e99fb30e831e7bc172d3e772181e6b62744f
Anton P <antonkhvdv@gmail.com>
2016-10-04 18:18:02 +0400

    url file added

---
+++ FoodAndRecipes/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url, include
+from patterns import patterns
+
+from . import views
+from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^$', views.recipe_list, name='recipe_list'),
+    url(r'^recipe/(?P<pk>[0-9]+)/$', views.recipe_detail, name='recipe_detail'),
+    url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
+    url(r'^recipe/(?P<pk>[0-9]+)/edit/$', views.recipe_edit, name='recipe_edit'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^search-form/$', views.search_form, name='search-form')
+]
+
+
+if settings.DEBUG:
+
+    if settings.MEDIA_ROOT:
+
+        urlpatterns += static(settings.MEDIA_URL,
+
+            document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
