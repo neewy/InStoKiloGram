d169c7baaa893ce7a88affca4364e595dc472bdf
Evgenia Lyashenko <evgenia-ya-ya@yandex.ru>
2016-10-11 15:09:28 +0300

    [MSD-33] - Moving "profile" to Users scheme, first commit

--- Users/forms.py
+++ Users/forms.py
@@ -13,3 +13,13 @@ class RegisterForm(forms.Form):
 class AccountForm(forms.Form):
     firstname = forms.CharField(label='First Name', max_length=100)
     lastname = forms.CharField(label='Last Name', max_length=100)
+    gender = forms.CharField(label='Gender', max_length = 6)
+    start_weight = forms.DecimalField(label='Current weight', max_digits=3, decimal_places=2)
+    height = forms.DecimalField(label='Height', max_digits=3, decimal_places=2)
+    birth_date = forms.DateField(label='Date of birth')
+    activity_level = forms.CharField(label='Activity level', max_length=2)
+    goal_weight = forms.DecimalField(label='Weight goals', max_digits=3, decimal_places=2)
+    # diet_goals = forms.CharField(label='Diet goals',max_length=100, choices=goals_choices)
+
+
+
--- Users/models.py
+++ Users/models.py
@@ -16,15 +16,28 @@ class User(AbstractUser):
     surname = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100)
     dateOfBirth = models.CharField(max_length=100)
-
     user_type = models.CharField(max_length=2,
                                  choices=type_choices,
                                  default='A')
+
 
 class UserProfile(models.Model):
     gender_choices = (
         ('M', 'Male'),
         ('F', 'Female'),
+    )
+    activity_choices = (
+        ('S', 'Sedentary'),
+        ('LA', 'Low active'),
+        ('A', 'Active'),
+        ('VA', 'Very active'),
+    )
+    goals_choices = (
+        ('MWG', 'Moderate weight gain'),
+        ('GWG', 'Gradual weight gain'),
+        ('MCW', 'Maintain current weight'),
+        ('GWL', 'Gradual weight loss'),
+        ('MWL', 'Moderate weight loss'),
     )
 
     gender = models.CharField(max_length=1,
@@ -34,3 +47,21 @@ class UserProfile(models.Model):
     start_weight = models.DecimalField(max_digits=3,
                                        decimal_places=2
                                        )
+    current_weight = models.DecimalField(max_digits=3,
+                                         decimal_places=2,
+                                         default=start_weight
+                                         )
+    goal_weight = models.DecimalField(max_digits=3,
+                                      decimal_places=2,
+                                      )
+    height = models.DecimalField(max_digits=3,
+                                 decimal_places=2,
+                                 )
+    birth_date = models.DateField
+
+    activity_level = models.CharField(max_length=2,
+                                      choices=activity_choices
+                                      )
+    diet_goals = models.CharField(max_length=100,
+                                  choices=goals_choices
+                                  )
