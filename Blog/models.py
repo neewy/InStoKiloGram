f8d34d5801deebd13f948698fca3bdd8d785880d
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2016-10-17 14:32:51 +0300

    MSD-36 Added basic design for main template and blog

--- Blog/models.py
+++ Blog/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
+
+import pretty
 from django.db import models
 from django.utils import timezone
-
 
 class Post(models.Model):
     author = models.ForeignKey('Users.User')
@@ -9,11 +10,24 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    image = models.ImageField(upload_to='pic_folder/')
+    image = models.ImageField(upload_to='pic_folder/', blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
+    def get_pretty_date(self):
+        time = timezone.make_naive(self.published_date)
+        return pretty.date(time, short=True)
+
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=20)
+    tag_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.tag_name
--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+
 from .forms import PostForm
-from django.shortcuts import redirect
+from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(post_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
