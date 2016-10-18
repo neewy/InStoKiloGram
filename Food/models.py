0677ca20959ccf47b852481d2dec6177af6181d5
Anton P <antonkhvdv@gmail.com>
2016-10-18 17:18:12 +0400

    MSD-42 CRUD for food    - Food Model Created

---
+++ Food/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    calories = models.IntegerField()
+    image = models.ImageField(upload_to='pic_folder/')
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
